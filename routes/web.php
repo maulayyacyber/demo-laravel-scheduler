b2e7da802f56b88b62e5146863d69b7821ab9dd7
maulayyacyber <ridaulmaulayya@gmail.com>
2024-01-08 12:23:59 +0700

    add route for debug post

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post', function () {
+    $post = App\Models\Post::find(1);
+    return $post;
+});
